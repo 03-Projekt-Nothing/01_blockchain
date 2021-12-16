8ee4e4246ae352a5537e35f073475e158be1f7a2
Dog-Of-War <plc-booking@gmx.de>
2021-12-16 11:28:45 +0000

    Changed somethings

--- dev-test.js
+++ dev-test.js
@@ -1,9 +1,16 @@
 const Block = require("./block");
+const Blockchain = require("./blockchain");
 
 // const block = new Block("time","hash1","hash2","data");
 // console.log(block.toString());
 
 // console.log(Block.genesis().toString());
 
-const testBlock =Block.mineBlock(Block.genesis(),"test");
-console.log(testBlock.toString());
+// const testBlock =Block.mineBlock(Block.genesis(),"test");
+// console.log(testBlock.toString());
+
+const blockchain = new Blockchain();
+console.log(blockchain);
+
+blockchain.addBlock("100,- € an Boo, 50,-€ von Foo");
+console.log(blockchain);
